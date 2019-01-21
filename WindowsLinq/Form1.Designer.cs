7dd25b8f1b9abc104cac88a256ab30c2858de182
hacer <hacer.346181@gmail.com>
2019-01-21 09:51:24 +0300

    Linq denemesi

---
+++ WindowsLinq/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace WindowsLinq
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.OrderGrid = new System.Windows.Forms.DataGridView();
+            this.OrderDetailGrid = new System.Windows.Forms.DataGridView();
+            this.SalesGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.OrderGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrderDetailGrid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SalesGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(214, 342);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.OrderDetailGrid);
+            this.groupBox1.Controls.Add(this.OrderGrid);
+            this.groupBox1.Location = new System.Drawing.Point(243, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(371, 356);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Siparişler";
+            // 
+            // OrderGrid
+            // 
+            this.OrderGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.OrderGrid.Location = new System.Drawing.Point(20, 33);
+            this.OrderGrid.Name = "OrderGrid";
+            this.OrderGrid.Size = new System.Drawing.Size(330, 150);
+            this.OrderGrid.TabIndex = 0;
+            // 
+            // OrderDetailGrid
+            // 
+            this.OrderDetailGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.OrderDetailGrid.Location = new System.Drawing.Point(20, 189);
+            this.OrderDetailGrid.Name = "OrderDetailGrid";
+            this.OrderDetailGrid.Size = new System.Drawing.Size(330, 150);
+            this.OrderDetailGrid.TabIndex = 1;
+            // 
+            // SalesGrid
+            // 
+            this.SalesGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SalesGrid.Location = new System.Drawing.Point(656, 12);
+            this.SalesGrid.Name = "SalesGrid";
+            this.SalesGrid.Size = new System.Drawing.Size(271, 342);
+            this.SalesGrid.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(939, 450);
+            this.Controls.Add(this.SalesGrid);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.OrderGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrderDetailGrid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SalesGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView OrderDetailGrid;
+        private System.Windows.Forms.DataGridView OrderGrid;
+        private System.Windows.Forms.DataGridView SalesGrid;
+    }
+}
+
---
+++ WindowsLinq/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsLinq
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DataNorthWindDataContext db = new DataNorthWindDataContext();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            var sales = db.Sales_by_Year(new DateTime(1990,1,1),new DateTime(2000,1,1));
+            SalesGrid.DataSource = sales;
+            var q = from c in db.Customers  select new { c.CustomerID, c.CompanyName };
+            dataGridView1.DataSource = q;
+        }
+    }
+}
